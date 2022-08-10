3d520117110079aac52ac26ff1ead8b40d761706
tmsquare <mouhamedmangassouba101@gmail.com>
2022-08-10 13:44:58 +0200

    Read benchmark cache miss on filestore

--- evaluation/read/read_request.sh
+++ evaluation/read/read_request.sh
@@ -6,11 +6,14 @@ if [ $# -ne 2 ]; then
 fi
 
 ncalls=$2
-command="curl  -L http://$1:31112/function/get-user-by-userid/accountId/4d2a46c7-71cb-4cf1-b5bb-b68406d9da6f"
+command="curl  -L http://$1:31112/function/get-user-by-userid-filestore/accountId/4d2a46c7-71cb-4cf1-b5bb-b68406d9da6a"
+
+idList=("4d2a46c7-71cb-4cf1-b5bb-b68406d9da6a" "4d2a46c7-71cb-4cf1-b5bb-b68406d9da6b" "4d2a46c7-71cb-4cf1-b5bb-b68406d9da6c" "4d2a46c7-71cb-4cf1-b5bb-b68406d9da6d" "4d2a46c7-71cb-4cf1-b5bb-b68406d9da6e" "4d2a46c7-71cb-4cf1-b5bb-b68406d9da6f")
+id=${idList[ $RANDOM % ${#idList[@]} ]}
 
 for i in `seq 2 ${ncalls}`
 do
-  command=${command}" -: http://$1:31112/function/get-user-by-userid/accountId/4d2a46c7-71cb-4cf1-b5bb-b68406d9da6f"
+  command=${command}" -: http://$1:31112/function/get-user-by-userid-filestore/accountId/$id"
 done
 
 eval $command
